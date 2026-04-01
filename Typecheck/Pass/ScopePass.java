299b045774071e1ce345473a361305fcff65bc31
trinhhdinhh <trdinh321@gmail.com>
2026-04-01 15:44:14 -0500

    Person 1 completed

--- Typecheck/Pass/ScopePass.java
+++ Typecheck/Pass/ScopePass.java
@@ -15,22 +15,47 @@ public class ScopePass<T> extends Pass<T> {
 
    @Override
    public T visitFunDecl(FunDecl node) {
+	Scope saved = currentscope;
+	currentscope = node.scope;
+	super.visitFunDecl(node);
+	currentscope = saved;
+	return defaultReturn;
    }
 
    @Override
 	public T visitStructDecl(StructDecl node) {
+		Scope saved = currentscope;
+		currentscope = node.scope;
+		super.visitStructDecl(node);
+		currentscope = saved;
+		return defaultReturn;
 	}
 
 	@Override
 	public T visitUnionDecl(UnionDecl node) {
+		Scope saved = currentscope;
+		currentscope = node.scope;
+		super.visitUnionDecl(node);
+		currentscope = saved;
+		return defaultReturn;
 	}
 
 	@Override
 	public T visitIfStmt(IfStmt node) {
+		Scope saved = currentscope;
+		currentscope = node.scope;
+		super.visitIfStmt(node);
+		currentscope = saved;
+		return defaultReturn;
 	}
 
    @Override
 	public T visitWhileStmt(WhileStmt node) {
+		Scope saved = currentscope;
+		currentscope = node.scope;
+		super.visitWhileStmt(node);
+		currentscope = saved;
+		return defaultReturn;
 	}
 
 }
